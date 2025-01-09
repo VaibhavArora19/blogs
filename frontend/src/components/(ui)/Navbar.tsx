9f53c9aa875aad81a2da3748992652daf7c72f68
Vaibhav Arora <aroravaibhav817@gmail.com>
2025-01-09 19:47:18 +0530

    feat: add navbar routes

--- frontend/src/components/(ui)/Navbar.tsx
+++ frontend/src/components/(ui)/Navbar.tsx
@@ -1,17 +1,19 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { Text } from "@chakra-ui/react";
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
     <div className="flex justify-between mt-8">
-      <Text textStyle="2xl" className="font-bold cursor-pointer">
+      <Text textStyle="2xl" className="font-bold cursor-pointer" onClick={() => router.push("/")}>
         Vaibhav&apos;s Blog
       </Text>
       <div className="flex gap-4">
-        <Text textStyle={"md"} className="p-2 cursor-pointer">
+        <Text textStyle={"md"} className="p-2 cursor-pointer" onClick={() => router.push("/https://vaibhavcodes.vercel.app/about")}>
           About
-        </Text>
-        <Text textStyle={"md"} className="p-2 cursor-pointer">
-          Archive
         </Text>
       </div>
     </div>

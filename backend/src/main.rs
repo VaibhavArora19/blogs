8b893b8fd3bc2f23dc58ecc5c5e06ea7e47a0d0e
Vaibhav Arora <aroravaibhav817@gmail.com>
2025-01-23 16:22:51 +0530

    feat: add dotenv

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,24 @@
 mod routes;
 mod services;
 
+extern crate dotenv;
+
 use actix_web::{App, HttpServer};
+use dotenv::dotenv;
 use routes::blogs_route::{get_blog, get_blogs};
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let port = match env::var("PORT") {
+        Ok(val) => val.parse::<u16>().unwrap(),
+        Err(_) => 8000,
+    };
+
     return HttpServer::new(|| App::new().service(get_blog).service(get_blogs))
-        .bind(("localhost", 8000))?
+        .bind(("localhost", port))?
         .run()
         .await;
 }
